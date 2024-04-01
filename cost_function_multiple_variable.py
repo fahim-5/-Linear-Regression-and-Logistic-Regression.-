a6e4c674a316d2fbea39ed3d986586f8d18af6ed
Fahim Faysal <164730091+fahim-5@users.noreply.github.com>
2024-04-01 22:56:24 +0600

    Add files via upload

---
+++ cost_function_multiple_variable.py
@@ -0,0 +1,19 @@
+def compute_cost(X, y, w, b): 
+    """
+    compute cost
+    Args:
+      X (ndarray (m,n)): Data, m examples with n features
+      y (ndarray (m,)) : target values
+      w (ndarray (n,)) : model parameters  
+      b (scalar)       : model parameter
+      
+    Returns:
+      cost (scalar): cost
+    """
+    m = X.shape[0]
+    cost = 0.0
+    for i in range(m):                                
+        f_wb_i = np.dot(X[i], w) + b           #(n,)(n,) = scalar (see np.dot)
+        cost = cost + (f_wb_i - y[i])**2       #scalar
+    cost = cost / (2 * m)                      #scalar    
+    return cost
